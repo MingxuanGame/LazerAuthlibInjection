5d811157da99c203a2c08b02879427032a161790
MingxuanGame <MingxuanGame@outlook.com>
2026-07-09 18:50:05 +0800

    Update `ppy.osu.Game`

--- osu.Game.Rulesets.EnhancedAuth/EnhancedAuthDifficultyCalculator.cs
+++ osu.Game.Rulesets.EnhancedAuth/EnhancedAuthDifficultyCalculator.cs
@@ -10,15 +10,14 @@ namespace osu.Game.Rulesets.EnhancedAuth
     public class EnhancedAuthDifficultyCalculator(IRulesetInfo ruleset, IWorkingBeatmap beatmap)
         : DifficultyCalculator(ruleset, beatmap)
     {
-        protected override DifficultyAttributes CreateDifficultyAttributes(IBeatmap beatmap, Mod[] mods, Skill[] skills,
-            double clockRate)
+        protected override DifficultyAttributes CreateDifficultyAttributes(IBeatmap beatmap, Mod[] mods, Skill[] skills)
         {
             return new DifficultyAttributes(mods, 0);
         }
 
         protected override IEnumerable<DifficultyHitObject> CreateDifficultyHitObjects(IBeatmap beatmap,
-            double clockRate) => [];
+                                                                                       Mod[] mods) => [];
 
-        protected override Skill[] CreateSkills(IBeatmap beatmap, Mod[] mods, double clockRate) => [];
+        protected override Skill[] CreateSkills(IBeatmap beatmap, Mod[] mods) => [];
     }
 }
